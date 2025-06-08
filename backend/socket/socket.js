dc2a9ffd0cc7ccaaf9347bb14ea63c0882d50c3f
Shriyam802 <cseshriyam802@gmail.com>
2025-06-08 12:56:41 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000', 'http://localhost:3004', 'http://localhost:3007', 'http://localhost:8080','https://realtime-caht-application-y6cm.vercel.app/login'],
+        origin:['http://localhost:3000', 'http://localhost:3004', 'http://localhost:3007', 'http://localhost:8080','https://realtime-caht-application-y6cm.vercel.app'],
         methods:['GET', 'POST'],
         credentials: true
     },
